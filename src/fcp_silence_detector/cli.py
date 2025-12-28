e1cf9436d47e37ded46f70225e5032b7a6ba9b65
leesugil <sugilmath@gmail.com>
2025-12-28 10:44:26 -0500

    default parameter update

--- src/fcp_silence_detector/cli.py
+++ src/fcp_silence_detector/cli.py
@@ -50,7 +50,7 @@ def main():
     parser.add_argument("--db", type=float, default=-45.0, help="Silence threshold in dB")
     parser.add_argument("--duration", type=float, default=3.5, help="Minimum silence duration in seconds")
     parser.add_argument("--polish_duration", type=float, default=0.7, help="Maximum non-silence duration in seconds")
-    parser.add_argument("--buffer_duration", type=float, default=2.0, help="Amount to reduce silence duration in seconds. (Should be greater than duration)")
+    parser.add_argument("--buffer_duration", type=float, default=1.5, help="Amount to reduce silence duration in seconds. (Should be greater than duration)")
     parser.add_argument("--sync", type=float, default=0, help="(experimental) synched clip.")
     args = parser.parse_args()
 
