4cf3c4f6f17a4f19411dc51921c48798dfecd259
leesugil <sugilmath@gmail.com>
2026-03-14 00:48:10 -0400

    minor update

--- src/fcp_silence_detector/place_markers.py
+++ src/fcp_silence_detector/place_markers.py
@@ -8,19 +8,37 @@ def place(root, silences: list[dict], fps: str, keyword: str, in_event: bool=Fal
     silences: [{'start': xx.xxx, 'end': yy.yyy, 'duration': zz.zzz}, {...}, ...]
     """
     asset_clip = fcpxml_io.get_event_asset_clip(root) if in_event else fcpxml_io.get_spine_asset_clip(root)
+    audio_channel = asset_clip.find('audio-channel-source')
+    if audio_channel is not None:
+        print("🐝 audio-channel-source found!")
 
     # Place silence Markers
     for i, s in tqdm(enumerate(silences, start=1)):
         start = arithmetic.float2fcpsec(s['start'], fps)
-        start_marker = ET.SubElement(asset_clip, "marker")
+        start_marker = ET.Element("marker")
         start_marker.set("start", start)
         start_marker.set("value", f"{keyword} start {i}")
         start_marker.set("duration", fps)
         start_marker.set("completed", "0")
 
+        if audio_channel is not None:
+            #print("🐝 audio-channel-source found!")
+            index = list(asset_clip).index(audio_channel)
+            asset_clip.insert(index, start_marker)
+        else:
+            #print("💀 NO audio-channel-source found!")
+            #print(f"{list(asset_clip)[-5:]}")
+            asset_clip.append(start_marker)
+
         end = arithmetic.float2fcpsec(s['end'], fps)
-        end_marker = ET.SubElement(asset_clip, "marker")
+        end_marker = ET.Element("marker")
         end_marker.set("start", end)
         end_marker.set("value", f"{keyword} end {i}")
         end_marker.set("duration", fps)
         end_marker.set("completed", "0")
+
+        if audio_channel is not None:
+            asset_clip.insert(index+1, end_marker)
+        else:
+            asset_clip.append(end_marker)
+
